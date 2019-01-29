399f05eaf17cb5efbb2779f6a248c8066e33784a
Abdul Mutholib Krisma Susanto <46935516+KrismaFx@users.noreply.github.com>
2019-01-30 06:51:42 +0700

    Add files via upload

---
+++ MYProfile.java
@@ -0,0 +1,15 @@
+// Dev By Krisma
+
+public class MYProfile {
+    public static void main(String[] args) {
+        
+        // This my profile
+        System.out.println("========== MY PROFILE ==========");
+        System.out.println("My Name is Krisma");
+        System.out.println("Full Name is Abdul Mutholib Krisma Susanto");
+        System.out.println("I`M From Indonesia");
+        System.out.println("20 Years Age");
+        System.out.println("Study in Politeknik Negeri Jakarta (D4)");
+        
+    }
+}
